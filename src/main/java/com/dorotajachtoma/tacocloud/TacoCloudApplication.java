ef186785ec96424f96d3047df8ab567afc4d1d6a
jachtomadorota <jachtoma.do@gmail.com>
2021-03-25 15:35:50 +0100

    initial commit form other source

--- src/main/java/com/dorotajachtoma/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/dorotajachtoma/tacocloud/TacoCloudApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TacoCloudApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
     }
