8a8cd60d4b3cd391c0b75d637e38d9880ef71eb8
jachtomadorota <jachtoma.do@gmail.com>
2021-03-25 17:33:11 +0100

    add validation tags for varibles in model classes

--- src/main/java/com/dorotajachtoma/tacocloud/model/Taco.java
+++ src/main/java/com/dorotajachtoma/tacocloud/model/Taco.java
@@ -4,13 +4,18 @@ package com.dorotajachtoma.tacocloud.model;
 import lombok.Data;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 @RequestMapping
 public class Taco {
 
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
