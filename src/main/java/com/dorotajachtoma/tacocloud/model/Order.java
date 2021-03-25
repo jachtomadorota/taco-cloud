7cb44647cdc111805215b3f9d515aca1ccd791a2
jachtomadorota <jachtoma.do@gmail.com>
2021-03-25 17:02:04 +0100

    enhance orderForm with functionalities and add Order model entity

--- src/main/java/com/dorotajachtoma/tacocloud/model/Order.java
+++ src/main/java/com/dorotajachtoma/tacocloud/model/Order.java
@@ -7,4 +7,20 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 public class Order {
+
+    private String name;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zip;
+
+    private String ccNumber;
+
+    private String ccExpiration;
+
+    private String ccCVV;
 }
