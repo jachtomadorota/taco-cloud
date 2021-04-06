e3b3242b02c9d14a4abf2ea5765fa8e223fde372
jachtomadorota <jachtoma.do@gmail.com>
2021-04-06 20:38:15 +0200

    add basic in-memory authentication crrdentials

--- src/main/java/com/dorotajachtoma/tacocloud/SecurityConfig.java
+++ src/main/java/com/dorotajachtoma/tacocloud/SecurityConfig.java
@@ -1,10 +1,24 @@
 package com.dorotajachtoma.tacocloud;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig implements WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("dorota")
+                .password("dorota")
+                .authorities("ROLE_USER");
+    }
 }
