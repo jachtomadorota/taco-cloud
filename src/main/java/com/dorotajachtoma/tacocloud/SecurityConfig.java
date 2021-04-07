bbdda28fef3bda4e7d1e391afd4a8b59e8406c5f
jachtomadorota <jachtoma.do@gmail.com>
2021-04-07 16:41:25 +0200

    add new UserDetailService to SecurityConfig class

--- src/main/java/com/dorotajachtoma/tacocloud/SecurityConfig.java
+++ src/main/java/com/dorotajachtoma/tacocloud/SecurityConfig.java
@@ -1,15 +1,26 @@
 package com.dorotajachtoma.tacocloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new Pbkdf2PasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorities("ROLE_USER");
 
        */
-
+        /*
         auth.ldapAuthentication()
                 .userSearchBase("ou=people")
                 .userSearchFilter("(uid={0})")
@@ -35,5 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .contextSource()
                 .root("dc=taco-cloud,dv=com");
+
+         */
+        auth.userDetailsService(userDetailsService)
+        .passwordEncoder(passwordEncoder());
     }
 }
