2672ac990ffc57c6d1c4b09a23bf8af9daf2f007
jachtomadorota <jachtoma.do@gmail.com>
2021-04-07 15:42:23 +0200

    add custom implementation of UserDetail iterface in model entity User, add configuration of LDAP server for authorization and authentication

--- src/main/java/com/dorotajachtoma/tacocloud/SecurityConfig.java
+++ src/main/java/com/dorotajachtoma/tacocloud/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
+      /*  auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password("admin")
                 .authorities("ROLE_USER")
@@ -20,5 +21,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("dorota")
                 .password("dorota")
                 .authorities("ROLE_USER");
+
+       */
+
+        auth.ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchFilter("(uid={0})")
+                .userSearchBase("ou=groups")
+                .userSearchFilter("member={0}")
+                .passwordCompare()
+                .passwordEncoder(new Pbkdf2PasswordEncoder())
+                .passwordAttribute("passcode")
+                .and()
+                .contextSource()
+                .root("dc=taco-cloud,dv=com");
     }
 }
