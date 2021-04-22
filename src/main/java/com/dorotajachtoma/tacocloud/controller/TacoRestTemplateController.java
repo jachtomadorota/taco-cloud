56d8be0452b14009587fbf2f1cba63f6e5dcbe9b
jachtomadorota <jachtoma.do@gmail.com>
2021-04-22 15:25:37 +0200

    add new Http method in TacoRestTemplateController using URI and HashMap

--- src/main/java/com/dorotajachtoma/tacocloud/controller/TacoRestTemplateController.java
+++ src/main/java/com/dorotajachtoma/tacocloud/controller/TacoRestTemplateController.java
@@ -4,9 +4,15 @@ package com.dorotajachtoma.tacocloud.controller;
 import com.dorotajachtoma.tacocloud.model.Ingredient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -21,7 +27,16 @@ public class TacoRestTemplateController {
     }
 
     public Ingredient getIngredientById(String ingredientId){
-        return template.getForObject("http://localhost:8080/taco-rest-template",Ingredient.class,ingredientId);
+        return template.getForObject("http://localhost:8080/taco-rest-template/{id}",Ingredient.class,ingredientId);
+    }
+
+    @GetMapping(value = "/hashmap")
+    public Ingredient getIngredientByIdHashMap(String ingredientId){
+        Map<String,String> urlVaribles = new HashMap<>();
+        urlVaribles.put("id",ingredientId);
+        URI uri = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/taco-rest-template/hashmap/{id}")
+                .build(urlVaribles);
+        return template.getForObject(uri,Ingredient.class);
     }
 
 
