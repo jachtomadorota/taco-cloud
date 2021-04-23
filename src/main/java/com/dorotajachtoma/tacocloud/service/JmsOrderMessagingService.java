658e0b6cb5c8c95344b9c33063a07b20437b6b74
jachtomadorota <jachtoma.do@gmail.com>
2021-04-23 11:21:10 +0200

    add implementation of method sendOrder(TacoOrder order) form interface OrderMessagingService

--- src/main/java/com/dorotajachtoma/tacocloud/service/JmsOrderMessagingService.java
+++ src/main/java/com/dorotajachtoma/tacocloud/service/JmsOrderMessagingService.java
@@ -2,12 +2,33 @@ package com.dorotajachtoma.tacocloud.service;
 
 
 import com.dorotajachtoma.tacocloud.model.TacoOrder;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 
 @Service
 public class JmsOrderMessagingService implements OrderMessagingService {
+
+    private final JmsTemplate jms;
+
+    public JmsOrderMessagingService(JmsTemplate jms) {
+        this.jms = jms;
+    }
+
     @Override
     public void sendOrder(TacoOrder order) {
+     /*   jms.send(new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                return session.createObjectMessage(order);
+            }
+        });
 
+      */
+        jms.send(session -> session.createObjectMessage(order));
     }
 }
