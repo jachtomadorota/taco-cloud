f32a8c528889099009460e09aa42b93d10a89c9d
jachtomadorota <jachtoma.do@gmail.com>
2021-04-23 11:36:14 +0200

    add new bean in JmsOrderMessagingService which sets default destination of this JMS configuration:

--- src/main/java/com/dorotajachtoma/tacocloud/service/JmsOrderMessagingService.java
+++ src/main/java/com/dorotajachtoma/tacocloud/service/JmsOrderMessagingService.java
@@ -2,16 +2,20 @@ package com.dorotajachtoma.tacocloud.service;
 
 
 import com.dorotajachtoma.tacocloud.model.TacoOrder;
+import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
+import javax.jms.Destination;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
 
 @Service
 public class JmsOrderMessagingService implements OrderMessagingService {
+
+    @Bean
+    public Destination orderQueue(){
+        return new ActiveMQQueue("taco-cloud.order.queue");
+    }
 
     private final JmsTemplate jms;
 
@@ -21,14 +25,6 @@ public class JmsOrderMessagingService implements OrderMessagingService {
 
     @Override
     public void sendOrder(TacoOrder order) {
-     /*   jms.send(new MessageCreator() {
-            @Override
-            public Message createMessage(Session session) throws JMSException {
-                return session.createObjectMessage(order);
-            }
-        });
-
-      */
         jms.send(session -> session.createObjectMessage(order));
     }
 }
