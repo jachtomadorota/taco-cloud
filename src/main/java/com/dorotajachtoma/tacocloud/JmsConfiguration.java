a7402d5206c110cb158f9faa01ce146f33a6d357
jachtomadorota <jachtoma.do@gmail.com>
2021-04-24 18:05:16 +0200

    add new bean in JmsXonfiguration - MappingJackson2MessageCOnverter

--- src/main/java/com/dorotajachtoma/tacocloud/JmsConfiguration.java
+++ src/main/java/com/dorotajachtoma/tacocloud/JmsConfiguration.java
@@ -4,6 +4,7 @@ package com.dorotajachtoma.tacocloud;
 import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 
 import javax.jms.Destination;
 
@@ -13,6 +14,12 @@ public class JmsConfiguration {
     @Bean
     public Destination orderQueue(){
         return new ActiveMQQueue("taco-cloud.order.queue");
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter messageConverter(){
+        return new MappingJackson2MessageConverter();
+
     }
 
 }
