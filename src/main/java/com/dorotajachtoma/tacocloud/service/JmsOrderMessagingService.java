84003b4c7fa4764bc70a6f2b7534a4f98c1f1810
jachtomadorota <jachtoma.do@gmail.com>
2021-04-25 08:28:33 +0200

    add new implementation of senOrder(TacoOrder order) in JmsORderMessagingService

--- src/main/java/com/dorotajachtoma/tacocloud/service/JmsOrderMessagingService.java
+++ src/main/java/com/dorotajachtoma/tacocloud/service/JmsOrderMessagingService.java
@@ -5,8 +5,11 @@ import com.dorotajachtoma.tacocloud.model.TacoOrder;
 import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessagePostProcessor;
 import org.springframework.stereotype.Service;
 import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
 
 
 @Service
@@ -21,6 +24,14 @@ public class JmsOrderMessagingService implements OrderMessagingService {
 
     @Override
     public void sendOrder(TacoOrder order) {
-        jms.send(session -> session.createObjectMessage(order));
+      //  jms.send(session -> session.createObjectMessage(order));
+
+        jms.convertAndSend("taco-cloud.order.queue", order, new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws JMSException {
+                message.setStringProperty("X_ORDER_SOURCE","WEB");
+                return message;
+            }
+        });
     }
 }
